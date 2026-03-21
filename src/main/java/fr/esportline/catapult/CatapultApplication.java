8c9551c65e042a9e95b5c5b53f81a5b65cb15747
enimaloc <antoine@enimaloc.fr>
2026-03-21 13:17:40 +0100

    chore: initial project setup with Spring Boot 4 and GitHub Actions

---
+++ src/main/java/fr/esportline/catapult/CatapultApplication.java
@@ -0,0 +1,13 @@
+package fr.esportline.catapult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatapultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatapultApplication.class, args);
+    }
+
+}
