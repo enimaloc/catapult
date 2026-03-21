f43d20faccf4c153488d13058dcd07bdec34d842
enimaloc <antoine@enimaloc.fr>
2026-03-21 13:17:40 +0100

    chore: initial project setup with Spring Boot 4 and GitHub Actions

---
+++ src/test/java/fr/esportline/catapult/CatapultApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esportline.catapult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatapultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
