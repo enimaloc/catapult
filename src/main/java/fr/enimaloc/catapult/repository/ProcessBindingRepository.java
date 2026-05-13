3a81b34385ff96b92d44793a37db34aa0daf681d
enimaloc <antoine@enimaloc.fr>
2026-05-13 04:48:26 +0200

    feat: add ProcessBinding entity and repository

---
+++ src/main/java/fr/enimaloc/catapult/repository/ProcessBindingRepository.java
@@ -0,0 +1,17 @@
+package fr.enimaloc.catapult.repository;
+
+import fr.enimaloc.catapult.domain.ProcessBinding;
+import fr.enimaloc.catapult.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProcessBindingRepository extends JpaRepository<ProcessBinding, UUID> {
+
+    List<ProcessBinding> findByUserOrderByProcessNameAsc(UserAccount user);
+
+    Optional<ProcessBinding> findFirstByUserAndProcessNameIn(UserAccount user, Collection<String> processNames);
+}
