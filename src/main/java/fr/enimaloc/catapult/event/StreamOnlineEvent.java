ef4b7c26159aa6b3c16ee5e0245b0f05460ba2f7
enimaloc <antoine@enimaloc.fr>
2026-05-31 07:48:58 +0200

    chore: move field to transient

--- src/main/java/fr/enimaloc/catapult/event/StreamOnlineEvent.java
+++ src/main/java/fr/enimaloc/catapult/event/StreamOnlineEvent.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationEvent;
 @Getter
 public class StreamOnlineEvent extends ApplicationEvent {
 
-    private final UserAccount user;
+    private final transient UserAccount user;
 
     public StreamOnlineEvent(Object source, UserAccount user) {
         super(source);
