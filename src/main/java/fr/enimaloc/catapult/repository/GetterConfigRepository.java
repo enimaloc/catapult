021e448d8df9227141d558738606f9b7428d28f8
enimaloc <antoine@enimaloc.fr>
2026-06-05 08:11:54 +0200

    feat(admin): add findByUserAndProvider to GetterConfigRepository

--- src/main/java/fr/enimaloc/catapult/repository/GetterConfigRepository.java
+++ src/main/java/fr/enimaloc/catapult/repository/GetterConfigRepository.java
@@ -5,9 +5,12 @@ import fr.enimaloc.catapult.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface GetterConfigRepository extends JpaRepository<GetterConfig, UUID> {
 
     List<GetterConfig> findByUserOrderByPriorityAsc(UserAccount user);
+
+    Optional<GetterConfig> findByUserAndProvider(UserAccount user, GetterConfig.Provider provider);
 }
