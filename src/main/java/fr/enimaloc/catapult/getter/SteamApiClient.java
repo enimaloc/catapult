e75a7c65523cae6b94a231ae127a2fdaed5b070b
enimaloc <antoine@enimaloc.fr>
2026-06-06 04:29:16 +0200

    feat(steam): migrate SteamApiClient interface to CompletableFuture

--- src/main/java/fr/enimaloc/catapult/getter/SteamApiClient.java
+++ src/main/java/fr/enimaloc/catapult/getter/SteamApiClient.java
@@ -1,38 +1,48 @@
 package fr.enimaloc.catapult.getter;
 
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public interface SteamApiClient {
 
     record PlayerSummary(String gameId, String gameName) {}
 
-    default Optional<PlayerSummary> getPlayerSummary(String steamId) {
+    CompletableFuture<Optional<PlayerSummary>> getPlayerSummary(String steamId, String personalToken);
+
+    default CompletableFuture<Optional<PlayerSummary>> getPlayerSummary(String steamId) {
         return getPlayerSummary(steamId, null);
     }
 
-    Optional<PlayerSummary> getPlayerSummary(String steamId, String personalToken);
-
-    default Map<String, Optional<PlayerSummary>> getPlayerSummaries(Collection<String> steamIds) {
-        Map<String, Optional<PlayerSummary>> result = new LinkedHashMap<>();
-        for (String id : steamIds) {
-            result.put(id, getPlayerSummary(id));
-        }
-        return result;
+    /**
+     * Fetches summaries for multiple Steam IDs.
+     * Default: fans out to individual getPlayerSummary calls.
+     * RealSteamApiClient overrides this with a single batch HTTP call.
+     */
+    default CompletableFuture<Map<String, Optional<PlayerSummary>>> getPlayerSummaries(Collection<String> steamIds) {
+        List<CompletableFuture<Map.Entry<String, Optional<PlayerSummary>>>> futures = steamIds.stream()
+            .map(id -> getPlayerSummary(id).thenApply(ps -> Map.entry(id, ps)))
+            .toList();
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+            .thenApply(__ -> futures.stream()
+                .collect(Collectors.toMap(
+                    f -> f.join().getKey(),
+                    f -> f.join().getValue()
+                )));
     }
 
-    default boolean isProfilePublic(String steamId) {
-        return true;
+    default CompletableFuture<Boolean> isProfilePublic(String steamId) {
+        return CompletableFuture.completedFuture(true);
     }
 
-    default boolean isProfilePublic(String steamId, String personalToken) {
+    default CompletableFuture<Boolean> isProfilePublic(String steamId, String personalToken) {
         return isProfilePublic(steamId);
     }
 
-    default List<String> getOwnedGameIds(String steamId) {
-        return List.of();
+    default CompletableFuture<List<String>> getOwnedGameIds(String steamId) {
+        return CompletableFuture.completedFuture(List.of());
     }
 }
