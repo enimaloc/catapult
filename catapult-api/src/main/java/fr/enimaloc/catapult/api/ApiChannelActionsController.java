7a35ef69fa62fdc5b82afe6032b8511dbbba65fa
enimaloc <antoine@enimaloc.fr>
2026-06-10 12:27:42 +0200

    fix(api): add @Transactional to deleteSteamPersonalToken

--- catapult-api/src/main/java/fr/enimaloc/catapult/api/ApiChannelActionsController.java
+++ catapult-api/src/main/java/fr/enimaloc/catapult/api/ApiChannelActionsController.java
@@ -22,6 +22,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -219,6 +220,7 @@ public class ApiChannelActionsController {
         syncTokenToPool(channelUser, decryptedToken, body.shared());
     }
 
+    @Transactional
     @PostMapping("/settings/steam-personal-token/delete")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteSteamPersonalToken(
