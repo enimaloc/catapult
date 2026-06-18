c21d35f6981dbc2da7c55a6208d94f000531a5b5
enimaloc <antoine@enimaloc.fr>
2026-06-18 02:52:22 +0200

    feat(notifications): admin UI for /admin/notifications

---
+++ catapult-web/src/main/java/fr/enimaloc/catapult/web/AdminNotificationsPageController.java
@@ -0,0 +1,15 @@
+package fr.enimaloc.catapult.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/notifications")
+public class AdminNotificationsPageController {
+
+    @GetMapping
+    public String page() {
+        return "admin/notifications";
+    }
+}
