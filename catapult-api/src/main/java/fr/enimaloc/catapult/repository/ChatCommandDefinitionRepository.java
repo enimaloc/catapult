965f1354d1cbcf953a8edbf4043e0aefb1d5f502
enimaloc <antoine@enimaloc.fr>
2026-06-19 04:30:54 +0200

    feat(chat-commands): add ChatCommandDefinition entity and repository

---
+++ catapult-api/src/main/java/fr/enimaloc/catapult/repository/ChatCommandDefinitionRepository.java
@@ -0,0 +1,15 @@
+package fr.enimaloc.catapult.repository;
+
+import fr.enimaloc.catapult.domain.ChatCommandDefinition;
+import fr.enimaloc.catapult.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ChatCommandDefinitionRepository extends JpaRepository<ChatCommandDefinition, UUID> {
+    List<ChatCommandDefinition> findByUser(UserAccount user);
+    Optional<ChatCommandDefinition> findByUserAndName(UserAccount user, String name);
+    boolean existsByUserAndName(UserAccount user, String name);
+}
