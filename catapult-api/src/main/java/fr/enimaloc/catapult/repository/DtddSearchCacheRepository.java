5e7d1cdd72e17507c077818de490d3bfc9b98e50
enimaloc <antoine@enimaloc.fr>
2026-06-20 16:56:17 +0200

    feat(dtdd): add DtddSearchCache entity + repository

---
+++ catapult-api/src/main/java/fr/enimaloc/catapult/repository/DtddSearchCacheRepository.java
@@ -0,0 +1,7 @@
+package fr.enimaloc.catapult.repository;
+
+import fr.enimaloc.catapult.domain.DtddSearchCache;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DtddSearchCacheRepository extends JpaRepository<DtddSearchCache, String> {
+}
