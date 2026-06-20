6c110a79ea91f9cf942d4bf506df63793f54e56a
enimaloc <antoine@enimaloc.fr>
2026-06-20 17:47:44 +0200

    fix(dtdd): gate DtddService/DtddMappingService on dtdd.enabled to avoid breaking test context

--- catapult-api/src/main/java/fr/enimaloc/catapult/service/DtddMappingService.java
+++ catapult-api/src/main/java/fr/enimaloc/catapult/service/DtddMappingService.java
@@ -11,6 +11,7 @@ import fr.enimaloc.catapult.repository.DtddSearchCacheRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +24,7 @@ import java.util.Optional;
 
 @Service
 @RefreshScope
+@ConditionalOnBooleanProperty("dtdd.enabled")
 @RequiredArgsConstructor
 public class DtddMappingService {
 
