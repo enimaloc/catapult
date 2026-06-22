18d3732959b82431fc27fe965c7a1fb526e5c63d
enimaloc <antoine@enimaloc.fr>
2026-06-22 15:56:35 +0200

    chore(spike): add spring-websocket dependency for HTMX-over-WS PoC

--- catapult-web/build.gradle.kts
+++ catapult-web/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
     implementation("org.webjars.npm:chart.js:$chartjsVersion")
