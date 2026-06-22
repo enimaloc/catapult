d8f10b7148773a5bb2c12d78c5a125f0767aba23
enimaloc <antoine@enimaloc.fr>
2026-06-22 16:17:51 +0200

    test(infra): verify spring-data-redis wiring with testcontainers

--- catapult-api/build.gradle.kts
+++ catapult-api/build.gradle.kts
@@ -73,6 +73,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
