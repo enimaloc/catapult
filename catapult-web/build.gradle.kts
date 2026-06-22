0b111e542adde3a64356a1529ae1a10b00929c4f
enimaloc <antoine@enimaloc.fr>
2026-06-22 16:10:58 +0200

    chore(deps): add spring-data-redis to api and web modules

--- catapult-web/build.gradle.kts
+++ catapult-web/build.gradle.kts
@@ -25,6 +25,7 @@ val htmxVersion = "2.0.4"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
