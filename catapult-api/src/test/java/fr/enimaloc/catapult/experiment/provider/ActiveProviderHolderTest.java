0897bad215b72d8d8abb9e174c13e164c85a3b7d
enimaloc <antoine@enimaloc.fr>
2026-07-05 18:05:22 +0200

    test: adapt ActiveProviderHolderTest to RestClient.Builder injection

--- catapult-api/src/test/java/fr/enimaloc/catapult/experiment/provider/ActiveProviderHolderTest.java
+++ catapult-api/src/test/java/fr/enimaloc/catapult/experiment/provider/ActiveProviderHolderTest.java
@@ -2,15 +2,17 @@ package fr.enimaloc.catapult.experiment.provider;
 
 import fr.enimaloc.catapult.config.ExperimentProviderProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 class ActiveProviderHolderTest {
 
     @Test
     void internalProvider_getReturnsNull_isInternalTrue() {
         var props = new ExperimentProviderProperties();
-        var holder = new ActiveProviderHolder(props);
+        var holder = new ActiveProviderHolder(props, mock(RestClient.Builder.class));
 
         assertThat(holder.get()).isNull();
         assertThat(holder.isInternal()).isTrue();
@@ -20,7 +22,7 @@ class ActiveProviderHolderTest {
     void unknownProvider_getReturnsNull_isInternalFalse() {
         var props = new ExperimentProviderProperties();
         props.setProvider("custom");
-        var holder = new ActiveProviderHolder(props);
+        var holder = new ActiveProviderHolder(props, mock(RestClient.Builder.class));
 
         assertThat(holder.get()).isNull();
         assertThat(holder.isInternal()).isFalse();
@@ -30,7 +32,7 @@ class ActiveProviderHolderTest {
     void growthbookProvider_getReturnsProvider() {
         var props = new ExperimentProviderProperties();
         props.setProvider("growthbook");
-        var holder = new ActiveProviderHolder(props);
+        var holder = new ActiveProviderHolder(props, mock(RestClient.Builder.class));
 
         assertThat(holder.get()).isNotNull();
         assertThat(holder.isInternal()).isFalse();
